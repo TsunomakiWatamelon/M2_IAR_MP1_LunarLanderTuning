428317a918da6404fc35f7be91605dc3b6d4de64
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2024-10-10 20:05:55 +0200

    plotting utils

--- src/tools.py
+++ src/tools.py
@@ -8,6 +8,9 @@ from torch.distributions import Normal
 from bbrl_utils.nn import build_mlp
 from bbrl.agents import Agent
 
+from scipy.stats import ttest_ind
+import matplotlib.pyplot as plt
+
 mse = nn.MSELoss()
 def compute_critic_loss(cfg, reward: torch.Tensor, must_bootstrap: torch.Tensor, q_values: torch.Tensor, target_q_values: torch.Tensor):
     """Compute the DDPG critic loss from a sample of transitions
@@ -73,3 +76,113 @@ class AddGaussianNoise(Agent):
         dist = Normal(act, self.sigma)
         action = dist.sample()
         self.set(("action", t), action)
+
+def compute_central_tendency_and_error(id_central, id_error, sample):
+
+    try:
+        id_error = int(id_error)
+    except:
+        pass
+
+    if id_central == "mean":
+        central = np.nanmean(sample, axis=1)
+    elif id_central == "median":
+        central = np.nanmedian(sample, axis=1)
+    else:
+        raise NotImplementedError
+
+    if isinstance(id_error, int):
+        low = np.nanpercentile(sample, q=int((100 - id_error) / 2), axis=1)
+        high = np.nanpercentile(sample, q=int(100 - (100 - id_error) / 2), axis=1)
+    elif id_error == "std":
+        low = central - np.nanstd(sample, axis=1)
+        high = central + np.nanstd(sample, axis=1)
+    elif id_error == "sem":
+        low = central - np.nanstd(sample, axis=1) / np.sqrt(sample.shape[0])
+        high = central + np.nanstd(sample, axis=1) / np.sqrt(sample.shape[0])
+    else:
+        raise NotImplementedError
+
+    return central, low, high
+
+def run_test(test_id, data1, data2, alpha=0.05):
+    """
+    Compute tests comparing data1 and data2 with confidence level alpha
+    :param test_id: (str) refers to what test should be used
+    :param data1: (np.ndarray) sample 1
+    :param data2: (np.ndarray) sample 2
+    :param alpha: (float) confidence level of the test
+    :return: (bool) if True, the null hypothesis is rejected
+    """
+    data1 = data1.squeeze()
+    data2 = data2.squeeze()
+    n1 = data1.size
+    n2 = data2.size
+
+    # perform Welch t-test":
+    _, p = ttest_ind(data1, data2, equal_var=False)
+    return p < alpha
+
+def perform_test(perf1, perf2, name1, name2, sample_size=20, downsampling_fact=5, confidence_level=0.01):
+    perf1 = perf1.transpose()
+    perf2 = perf2.transpose()
+    nb_datapoints = perf1.shape[1]
+    nb_steps = perf1.shape[0]
+
+    legend = [name1, name2]
+
+    # what do you want to plot ?
+    id_central = 'mean' # "median"  # 
+    id_error = 80  # (percentiles), also: 'std', 'sem'
+
+    test_id = "Welch t-test"  # recommended
+    
+    sample1 = perf1[:, np.random.randint(0, nb_datapoints, sample_size)]
+    sample2 = perf2[:, np.random.randint(0, nb_datapoints, sample_size)]
+
+    steps = np.arange(0, nb_steps, downsampling_fact)
+    sample1 = sample1[steps, :]
+    sample2 = sample2[steps, :]
+
+    # test
+    sign_diff = np.zeros([len(steps)])
+    for i in range(len(steps)):
+        sign_diff[i] = run_test(
+            test_id, sample1[i, :], sample2[i, :], alpha=confidence_level
+        )
+
+    central1, low1, high1 = compute_central_tendency_and_error(
+        id_central, id_error, sample1
+    )
+    central2, low2, high2 = compute_central_tendency_and_error(
+        id_central, id_error, sample2
+    )
+
+    # plot
+    _, ax = plt.subplots(1, 1, figsize=(20, 10))
+    lab1 = plt.xlabel("training steps")
+    lab2 = plt.ylabel("performance")
+
+    plt.plot(steps, central1, linewidth=10)
+    plt.plot(steps, central2, linewidth=10)
+    plt.fill_between(steps, low1, high1, alpha=0.3)
+    plt.fill_between(steps, low2, high2, alpha=0.3)
+    leg = ax.legend(legend, frameon=False)
+
+    # plot significative difference as dots
+    idx = np.argwhere(sign_diff == 1)
+    y = max(np.nanmax(high1), np.nanmax(high2))
+    plt.scatter(steps[idx], y * 1.05 * np.ones([idx.size]), s=100, c="k", marker="o")
+
+    # style
+    for line in leg.get_lines():
+        line.set_linewidth(10.0)
+    ax.spines["top"].set_linewidth(5)
+    ax.spines["right"].set_linewidth(5)
+    ax.spines["bottom"].set_linewidth(5)
+    ax.spines["left"].set_linewidth(5)
+
+    plt.savefig(
+        f"./{name1}_{name2}.pdf", bbox_extra_artists=(leg, lab1, lab2), bbox_inches="tight", dpi=100
+    )
+    plt.show()
